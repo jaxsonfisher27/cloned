edd56b77a12b05134170d1f5301141c792f498ce
jaxsonfisher27 <71225849+jaxsonfisher27@users.noreply.github.com>
2020-11-01 09:17:40 -0600

    Update while.sh

--- while.sh
+++ while.sh
@@ -1,3 +1,4 @@
+#Very similar to the counter function earlier in the project but this time counting upwards to a specified limit
 #!/bin/bash
 
 counter=2
@@ -5,3 +6,4 @@ while [ $counter -lt 3 ]; do
 	let counter+=1
 	echo $counter
 done
+#Same as the counter function, do and done are statements to start and finish the function
