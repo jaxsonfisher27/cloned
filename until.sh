7354691874c653d8323f9a138d38b00e071de11a
jaxsonfisher27 <71225849+jaxsonfisher27@users.noreply.github.com>
2020-11-01 09:13:52 -0600

    Update until.sh

--- until.sh
+++ until.sh
@@ -1,3 +1,4 @@
+#This block is a counter function that ticks down until a set value
 #!/bin/bash
 
 counter=4
@@ -5,4 +6,4 @@ until [ $counter -lt 3 ]; do
 	let counter-=1
 	echo $counter
 done
-
+#Whats interesting is the fact that until, do, and done are all defined and accepted conditional statements in shell
