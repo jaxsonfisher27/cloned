5f653b19fe322a4ca07f75397db3d2aa59648a16
jaxsonfisher27 <71225849+jaxsonfisher27@users.noreply.github.com>
2020-11-01 09:19:39 -0600

    Update param.sh

--- param.sh
+++ param.sh
@@ -1,5 +1,7 @@
+#This block simply lists the values stated in any specified way
 #!/usr/bin/bash
 
 echo $1 $2 $4
 echo $#
 echo $*
+#The use of the $# and $* simplifies any functions utilizing these values in comparison to javascript
