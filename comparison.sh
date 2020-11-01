c7a18c4c905d1c5a10c8170b4119bbca1b838038
jaxsonfisher27 <71225849+jaxsonfisher27@users.noreply.github.com>
2020-11-01 08:59:32 -0600

    Update comparison.sh

--- comparison.sh
+++ comparison.sh
@@ -1,3 +1,4 @@
+# This purpose of this block is to compare two different strings and two different numbers to see if they are equal and to return an output stating two questions and outputting an answer based on the values
 #!/bin/bash
 
 string_a="UNIX"
@@ -13,3 +14,4 @@ num_b=100
 echo "Is $num_a equal to $num_b ?"
 [ $num_a -eq $num_b ]
 echo $?
+#I found the use of $? intriguing as it outputs an answer based on the equation above, but it's very different in appearance in comparison to a java return ""; statement.
