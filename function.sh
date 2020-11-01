c32e8c9ee56736d08009637f4de3bc7430cca639
jaxsonfisher27 <71225849+jaxsonfisher27@users.noreply.github.com>
2020-11-01 09:04:09 -0600

    Update function.sh

--- function.sh
+++ function.sh
@@ -1,3 +1,4 @@
+#This block outputs the user name and directory path
 #!/bin/bash
 
 function user_details {
@@ -8,3 +9,4 @@ function user_details {
 user_details
 user_details
 
+#What I found interesting was that the function was performed twice to output both the username and directory
