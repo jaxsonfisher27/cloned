a02a523e9cda6e3c5738f02340b1b6c0aaa07587
jaxsonfisher27 <71225849+jaxsonfisher27@users.noreply.github.com>
2020-11-01 09:15:57 -0600

    Update welcome.sh

--- welcome.sh
+++ welcome.sh
@@ -1,3 +1,4 @@
+#This block is a greeting function to welcome the specified user and state the current day and bash version
 #!/bin/bash
 
 greeting="Welcome"
@@ -6,3 +7,4 @@ day=$(date +%A)
 
 echo "$greeting back $user! Today is $day, which is the best day of the entire week!"
 echo "Your Bash shell version is: $BASH_VERSION. Enjoy!"
+#What's interesting is that $BASH_VERSION is a command in and of itself that will enable users to see the stated info
