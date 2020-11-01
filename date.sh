c5c7d0302e41beda4312143104c67c8cd5a009d9
jaxsonfisher27 <71225849+jaxsonfisher27@users.noreply.github.com>
2020-11-01 09:01:47 -0600

    Update date.sh

--- date.sh
+++ date.sh
@@ -1,3 +1,5 @@
+#This block displays the date
 #!/bin/bash
 
 date
+#What I found interesting is how very simplified this code is in comparison to java, where you'd likely have to import some package to gain access to simplified direct date commands
